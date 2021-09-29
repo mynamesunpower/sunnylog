b1052ec4d14b0504009069ae18565733a63e0ab4
mynamesunpower <mynamesunpower@gmail.com>
2021-09-29 23:04:42 +0900

    getSession

--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Link from 'next/link';
 import Logo from './logo';
 import classes from './main-navigation.module.css';
+import { useSession, signOut } from 'next-auth/client';
 
 const MainNavigation: React.FC = () => {
+  const [session, loading] = useSession(); // loading -> 로딩 중인지.
+
+  const logoutHandler = () => {
+    signOut();
+  };
+
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -13,21 +20,27 @@ const MainNavigation: React.FC = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
           <li>
             <Link href="/posts">Posts</Link>
           </li>
           <li>
             <Link href="/contact">Contact</Link>
           </li>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,9 +1,25 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSession, getSession } from 'next-auth/client';
 
 const UserProfile: React.FC = () => {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = '/auth';
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
   return (
     <section className={classes.profile}>
       <h1>테스트</h1>
