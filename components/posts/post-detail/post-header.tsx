30115f5631125fc2735c65f00e901d93bb99df04
mynamesunpower <mynamesunpower@gmail.com>
2021-12-07 00:00:31 +0900

    Main 정리 중

--- components/posts/post-detail/post-header.tsx
+++ components/posts/post-detail/post-header.tsx
@@ -11,8 +11,8 @@ const PostHeader: React.FC<PostHeaderProps> = (props) => {
   const { title, image } = props;
   return (
     <header className={classes.header}>
-      <h1>{title}</h1>
       <Image src={image} alt={title} width={200} height={150} />
+      <h1>{title}</h1>
     </header>
   );
 };
